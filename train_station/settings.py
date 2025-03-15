454a29ceab8130659fefdfdbe722b6ec1bfb4a42
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-03-15 16:42:28 +0200

    add README.md with isnruction

--- train_station/settings.py
+++ train_station/settings.py
@@ -87,7 +87,16 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ["POSTGRES_DB"],
+#         "USER": os.environ["POSTGRES_USER"],
+#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+#         "HOST": os.environ["POSTGRES_HOST"],
+#         "PORT": os.environ["POSTGRES_PORT"],
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
